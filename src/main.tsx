de439fd3fc3f9c7cadff08f9c827ad0b3d2aaffe
wanda azhar <wandaazhar@gmail.com>
2025-06-04 18:51:25 -0600

    make it all empty (blank)

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import { DndProvider } from 'react-dnd'; // Import DndProvider
+import { HTML5Backend } from 'react-dnd-html5-backend'; // Import a backend
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <DndProvider backend={HTML5Backend}> {/* Wrap App with DndProvider */}
+      <App />
+    </DndProvider>
   </StrictMode>,
-)
+);
