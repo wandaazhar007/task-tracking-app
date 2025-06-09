9ff79d2eb7f3534acf7e0a11b03101e6256610bf
wanda azhar <wandaazhar@gmail.com>
2025-06-09 02:43:50 -0600

    update function navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,9 +1,38 @@
-import './navbar.scss';
+// src/components/navbar/Navbar.tsx
+/*
+Author: Wanda Azhar
+Location: Twin Falls, ID, USA
+Contact: wandaazhar@gmail.com
+Description: The main navigation bar for the application.
+*/
 
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTasks } from '@fortawesome/free-solid-svg-icons';
+import './navbar.scss';
 
 const Navbar = () => {
   return (
-    <h1>This is a navbar</h1>
+    <nav className="navbar">
+      <div className="navbar-container">
+        <Link to="/" className="navbar-logo">
+          <FontAwesomeIcon icon={faTasks} />
+          Task Tracker
+        </Link>
+        <ul className="nav-menu">
+          <li className="nav-item">
+            <Link to="/" className="nav-links">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/about" className="nav-links">
+              About
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
 
